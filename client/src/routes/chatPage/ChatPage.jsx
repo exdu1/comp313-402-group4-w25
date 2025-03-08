1769817128d95d188b911b3410b55cbe6866909f
Eric Du <ericxdu1@gmail.com>
2025-03-08 14:16:56 -0500

    Added divs to create skeleton for home page and chat page

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,27 @@
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './chatPage.css'
 
 const ChatPage = () => {
     return (
-        <div className='chatpage'>Chat Page</div>
-    )
-}
+        <div className="chat-page">
+            <div className="chat-container">
+                <div className="chat-header">
+                    <h2>Calming Echo Active Listener</h2>
+                    <p>How are you feeling today?</p>
+                </div>
+                
+                <div className="messages-container">
+
+                </div>
+
+                <form className="input-container" onSubmit= { handleSubmit }>
+                    <input />
+                    <button />
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default ChatPage;
--- client/src/routes/homepage/Homepage.jsx
+++ client/src/routes/homepage/Homepage.jsx
@@ -4,6 +4,9 @@ import './homepage.css'
 const Homepage = () => {
     return (
       <div className="homepage">
+        <div className="hero">
+          <h1>Welcome to Calming Echo, your AI Active Listening Companion</h1>
+        </div>
         <div className="actions">
             <Link to="/chat" className="cta-button">Start Chatting</Link>
         </div>
