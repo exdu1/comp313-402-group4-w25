6ee555607db15a947f8fb35237a0bdbb5dfd4b86
Eric Du <ericxdu1@gmail.com>
2025-03-08 12:04:59 -0500

    Added routing and button in frontend to link to chatpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import ChatPage from './routes/chatPage/ChatPage';
+import Homepage from './routes/homepage/Homepage';
+
 const App = () => {
-  return (
-    <div>Hello World</div>
-  )
+    return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/chat" element={<ChatPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+      </BrowserRouter>
+  )  
 }
 
 export default App
--- client/src/routes/homepage/Homepage.jsx
+++ client/src/routes/homepage/Homepage.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import './homepage.css'
 
 const Homepage = () => {
     return (
-        <div className='homepage'>Homepage</div>
-    )
-}
+      <div className="homepage">
+        <div className="actions">
+            <Link to="/chat" className="cta-button">Start Chatting</Link>
+        </div>
+      </div>  
+    );
+};
 
-export default Homepage
+export default Homepage;
