c7195201f81640a081aecc1a88f1b0a1531cbd7a
Eric Du <ericxdu1@gmail.com>
2025-04-12 13:24:04 -0400

    updated vite.config.js to handle envrionment variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
+  },
+  // For environment variable handling
+  define: {
+    'process.env': {}
+  }
 })
